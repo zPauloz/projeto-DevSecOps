b3ebd38854280c73512514dca380bcf48d09f31e
zPauloz <pereirapaulo59p@gmail.com>
2026-08-24 19:03:19 -0300

    fix: remove debug mode da aplicação

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return {
-        "message":"minha primeira API DevSecOps"
+        "message": "Minha primeira API DevSecOps"
     }
 
 
-if __name__=="__main__":
-    app.run(debug=True)
-    
+if __name__ == "__main__":
+    app.run()
